51db782a71cfd641e439bc19630b43540c177c8b
ElonMusk2018 <clogbenner@hotmail.com>
2019-02-04 10:26:47 -0800

    AA

---
+++ hello.c
@@ -0,0 +1,15 @@
+// hello.c
+// compile gcc hello.c -o hello.o
+// run ./hello.o
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello, World!");
+	printf("**********************************	 \n");
+	printf("	lb	lb					         \n");
+	printf("    lb	lb						     \n");
+	printf("    lb	lb					         \n");
+	printf("	lb	lb						     \n");
+	return 0;
+}
