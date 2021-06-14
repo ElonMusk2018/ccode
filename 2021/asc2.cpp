7965ac1580240f4a7ff17920594a3bd8faa051aa
Logan Benner <42662533+Logan-Benner@users.noreply.github.com>
2021-06-14 08:16:26 -0700

    Add files via upload

---
+++ 2021/asc2.cpp
@@ -0,0 +1,15 @@
+// Change line 8 so only charater 32 to 126 print
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	char c;
+	for(n = 0; n < 128; n++){
+		c = char(n);
+		cout<< n << "=" << c << " ";
+		if (n % 10 == 0)cout<<"\n";
+	}
+
+}
+
---
+++ 2021/helloworld.c
@@ -0,0 +1,10 @@
+// helloworld.c compile   
+// compile : gcc helloworld.c -o helloworld.o
+#include <stdio.h>
+
+int main(){
+	printf("Hello World\n");
+	printf("Hola Mundo\n");
+	printf("AAAAAAAAAAAAAAAAAAAA\n");
+	return 0;
+}
